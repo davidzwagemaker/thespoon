362c43ab41ab38bda839f70f79e5db974e877d70
David Zwagemaker <david@zwagemaker.nl>
2018-05-21 10:24:19 +0100

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # add / as homepages
+  root to: 'pages#home'
+  # get 'pages/home'
+
   get 'pages/contact'
-  get 'pages/about'
-  get 'pages/home'
+
+  # get 'pages/about' --> update pages/about to about
+  get 'about', to: 'pages#about', as: :about
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

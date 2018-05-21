257271794b60b2f5d8a83b60d75d59b485842bd2
David Zwagemaker <david@zwagemaker.nl>
2018-05-21 10:11:20 +0100

    added PagesController

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  # contact action,
+  # When you go to /pages/about, this method will be called
+  # and it will render the views/pages/about.html.erb
+  def contact
+  end
+
+  # about action
+  def about
+  end
+
+  # home action
+  def home
+  end
+end
